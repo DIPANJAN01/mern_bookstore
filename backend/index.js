294f3ce0b842151e6fd3aaf47a2e972344cfe0e8
Dipanjan <dipanjanghosal01@gmail.com>
2024-02-04 13:54:36 +0530

    Implemented 'POST/ books' endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import express from "express";
+import { Book } from "./models/bookModel.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => {
@@ -17,3 +20,20 @@ mongoose
     });
   })
   .catch(console.error);
+
+app.post("/books", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+    if (!title || !author || !publishYear) {
+      return res
+        .status(400)
+        .send("Title, Author and PublishYear of book required!");
+    }
+    const newBook = { title, author, publishYear };
+    const savedBook = await Book.create(newBook);
+    res.status(201).send(savedBook);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
