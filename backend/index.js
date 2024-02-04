f18d2073a8b541fccabb387d22982ab3189b0446
Dipanjan <dipanjanghosal01@gmail.com>
2024-02-04 15:59:38 +0530

    implemented cors policy

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,23 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import express from "express";
 import bookRouter from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 
 const app = express();
 
 //setup
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => {
