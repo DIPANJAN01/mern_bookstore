d4e035f4433a929a8b5449b1e54ac65ac1033a80
Dipanjan <dipanjanghosal01@gmail.com>
2024-02-04 15:58:42 +0530

    Organized the routes with Express Router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import express from "express";
-import { Book } from "./models/bookModel.js";
+import bookRouter from "./routes/bookRoutes.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+//setup
 
 app.use(express.json());
 mongoose
@@ -21,92 +23,4 @@ mongoose
   })
   .catch(console.error);
 
-//Post book
-app.post("/books", async (req, res) => {
-  try {
-    const { title, author, publishYear } = req.body;
-    if (!title || !author || !publishYear) {
-      return res
-        .status(400)
-        .send("Title, Author and PublishYear of book required!");
-    }
-    const newBook = { title, author, publishYear };
-    const savedBook = await Book.create(newBook);
-    res.status(201).send(savedBook);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-//Get all books
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find();
-    return res.status(200).send({
-      count: books.length,
-      books,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-//Get a book by id
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findById(id);
-    if (!book) {
-      return res.status(404).send("Book with the given id not found!");
-    }
-    return res.status(200).send({ book });
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).send({ message: error.message });
-  }
-});
-
-//Update book
-app.patch("/books/:id", async (req, res) => {
-  try {
-    const { title, author, publishYear } = req.body;
-    if (!title || !author || !publishYear) {
-      return res
-        .status(400)
-        .send("Title, Author and PublishYear of book required!");
-    }
-    const updatedBook = { title, author, publishYear };
-    const { id } = req.params;
-
-    const savedBook = await Book.findByIdAndUpdate(id, updatedBook, {
-      new: true,
-    });
-    if (!savedBook) {
-      return res.status(404).send("Book with the given id not found!");
-    }
-
-    res.status(201).send(savedBook);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-//Delete a book by id
-app.delete("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findByIdAndDelete(id);
-    if (!book) {
-      return res.status(404).send("Book with the given id not found!");
-    }
-    return res.status(201).send("Book deleted successfully!");
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).send({ message: error.message });
-  }
-});
+app.use("/books", bookRouter);
